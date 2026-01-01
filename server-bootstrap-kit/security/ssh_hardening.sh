4b7c410d4ddc2ae3338003adca5874323af387cd
Orgun <100226060+OrgunTheExplorer@users.noreply.github.com>
2026-01-01 13:14:48 +0300

    Enable PubkeyAuthentication in SSH configuration

--- server-bootstrap-kit/security/ssh_hardening.sh
+++ server-bootstrap-kit/security/ssh_hardening.sh
@@ -9,6 +9,8 @@ cp $SSH_CONFIG ${SSH_CONFIG}.backup
 sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin no/' $SSH_CONFIG
 sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/' $SSH_CONFIG
 sed -i 's/^#\?ChallengeResponseAuthentication.*/ChallengeResponseAuthentication no/' $SSH_CONFIG
+sed -i 's/^#\?PubkeyAuthentication.*/PubkeyAuthentication yes/' $SSH_CONFIG
+
 sed -i 's/^#\?UsePAM.*/UsePAM yes/' $SSH_CONFIG
 
 echo "Root login and password authentication disabled."
