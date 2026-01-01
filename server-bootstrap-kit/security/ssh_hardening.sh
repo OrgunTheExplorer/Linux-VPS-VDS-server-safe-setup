eba650029d5085a948332da53b28948bccf8dc1c
Orgun <100226060+OrgunTheExplorer@users.noreply.github.com>
2026-01-01 12:18:30 +0300

    Add SSH hardening script
    Backup the original SSH configuration and apply hardening settings.

---
+++ server-bootstrap-kit/security/ssh_hardening.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SSH_CONFIG="/etc/ssh/sshd_config"
+
+echo "Hardening SSH configuration..."
+
+cp $SSH_CONFIG ${SSH_CONFIG}.backup
+
+sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin no/' $SSH_CONFIG
+sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/' $SSH_CONFIG
+sed -i 's/^#\?ChallengeResponseAuthentication.*/ChallengeResponseAuthentication no/' $SSH_CONFIG
+sed -i 's/^#\?UsePAM.*/UsePAM yes/' $SSH_CONFIG
+
+echo "Root login and password authentication disabled."
